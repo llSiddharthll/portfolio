bb23d3c5638c50c143d6866df4a21026dc3ab657
llSiddharthll <siddharthgreat443@gmail.com>
2023-11-09 23:56:24 +0530

    Siddharth Portfolio

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,5 @@
 echo "BUILD START"
 pip3 install -r requirements.txt
-python3.9 manage.py makemigrations app
-python3.9 manage.py makemigrations account
-python3.9 manage.py makemigrations socialaccount
-python3.9 manage.py migrate app
-python3.9 manage.py migrate 
-python3.9 manage.py migrate account
-python3.9 manage.py migrate socialaccount
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
 echo "BUILD DONE"s
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
@@ -116,7 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'sid', 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
