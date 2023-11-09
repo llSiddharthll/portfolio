b793cc5ae04c459bf69704090cc0948ccb1f5f4b
llSiddharthll <siddharthgreat443@gmail.com>
2023-11-10 00:26:08 +0530

    again

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo "BUILD START"
-python3.9 -m pip3 install --upgrade pip3
+pip install --upgrade pip
 pip3 install -r requirements.txt
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
